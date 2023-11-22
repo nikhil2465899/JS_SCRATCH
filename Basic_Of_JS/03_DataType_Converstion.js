f27746ab39821ca4f747daff9a37cab77050ff45
Nikhil Sanjay Gurav <94897817+nikhil2465899@users.noreply.github.com>
2023-11-23 00:21:41 +0530

    Conversion_Changes

--- Basic_Of_JS/03_DataType_Converstion.js
+++ Basic_Of_JS/03_DataType_Converstion.js
@@ -1,3 +1,18 @@
 // First assign the vaule to the variable
 let testData = "Nikhil";
 console.log(typeof testData);
+
+// Now convert the number into boolean 
+let isNumber = 0;
+let booleanNumberValue = Boolean(isNumber);
+console.log(booleanNumberValue);
+
+// Now convert string number into number
+let stringNumber = "24";
+let numberInNumber = Number(stringNumber);
+console.log(numberInNumber);
+
+// Now convert boolean value into number
+let boolNumberValue = true;
+let boolValueInNumber = Number(boolNumberValue);
+console.log(boolValueInNumber);
