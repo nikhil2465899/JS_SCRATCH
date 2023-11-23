1b147c0ad9eeb3b6302201a7b1ff40151e4c5493
Nikhil Sanjay Gurav <94897817+nikhil2465899@users.noreply.github.com>
2023-11-23 23:34:49 +0530

    Changes_Basic

--- Basic_Of_JS/Basic.js
+++ Basic_Of_JS/Basic.js
@@ -1,3 +1,13 @@
 var obj = 'nikhil';
 
 console.log(obj);
+
+// Fun part of java script is 
+let stringTypeVariable = "1";
+let numberTypeVariable = 1;
+
+// Adding both variable 
+console.log(stringTypeVariable + numberTypeVariable); // It gives result (11)
+console.log(numberTypeVariable + stringTypeVariable); // It gives result (11)
+console.log(stringTypeVariable + stringTypeVariable); // It gives result (11)
+console.log(numberTypeVariable + numberTypeVariable); // It gives result (2)
