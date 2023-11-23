516de8cf3b3c0b316cc718e6ada68fa4d68ef628
Nikhil Sanjay Gurav <94897817+nikhil2465899@users.noreply.github.com>
2023-11-24 00:16:24 +0530

    Commit_DataType_Comparision

---
+++ Basic_Of_JS/04_DataType_Comparision.js
@@ -0,0 +1,16 @@
+// Check the string type number compare with number
+console.log("2"> 1); // It gives the result true. 
+
+// Now check the string type number with same type
+console.log("2" < "3"); // It gives the result true.
+// so we can compare the string number to each other 
+
+// Now checking for the null dataType
+console.log(null > 1); // It gives result false
+// Keep Note : null taking the value itself as 0 in comparision
+// Keep Note : Undefined is alo taking the value itself as 0 in comparision
+
+// Difference between == and ===
+// == Check the equality between value not dataType
+// === check the equality between value as well as dataType
+console.log("2" === 2); // It gives the false value
