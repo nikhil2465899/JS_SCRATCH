8c429c5b403a448cd2f38c093f7b36f895f5b310
Nikhil Sanjay Gurav <94897817+nikhil2465899@users.noreply.github.com>
2023-12-10 18:12:51 +0530

    Objects_methods

--- Objects_In_Js/01_Object.js
+++ Objects_In_Js/01_Object.js
@@ -22,3 +22,47 @@ const literalsObject = {
 console.log(literalsObject);
 
 /* Access object in two types */
+// First way
+console.log(literalsObject.Name);
+
+// Second way
+console.log(literalsObject["id"]);
+
+/* We can nested the objects into objects*/
+// For example 
+const nestedObjectExample = {
+  id:1,
+  name:"Nikhil",
+  Skills:{
+    languages: ["C", "c++"],
+    Database : "mongoDB"
+  }
+}
+console.log(nestedObjectExample);
+console.log(nestedObjectExample.Skills.languages);
+
+// Combine two object in one like merge of an array
+/* Syntax -> Object.assign(Target, source)*/
+const resultOfCombineObj = Object.assign({},nestedObjectExample, singletonObject)
+console.log(resultOfCombineObj);
+
+/* Some another properties of object that is we can freeze the object
+Now you make an object and you want the value doesn't change of that object then
+we have to freeze the object with current value
+*/
+// For example
+const freezeTheObject = {
+  id :5,
+  name: "nikhil"
+}
+
+// Freeze this object 
+Object.freeze(freezeTheObject);
+console.log(freezeTheObject); // { id: 5, name: 'nikhil' }
+
+// Try to manipulate the object
+freezeTheObject.id=6;
+console.log(freezeTheObject.id); // Still print the id= 5;
+
+// Return true becaue object is froozen 
+console.log(Object.isFrozen());
