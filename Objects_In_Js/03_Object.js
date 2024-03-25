4eb6b9e9962bb93ebb04b22d71093e768db8fc03
Nikhil Sanjay Gurav <94897817+nikhil2465899@users.noreply.github.com>
2024-03-25 17:18:55 +0530

    Changes in Object

---
+++ Objects_In_Js/03_Object.js
@@ -0,0 +1,89 @@
+/* Objects are mostly used in the get data from api or website.
+  We can store multiple type data in the object like name as a string , age as a number etc.
+
+  Basically object has two types singleton and literls
+  singleton -> classes which can be instantiated once, and can be accessed globally
+  literls  -> It is a syntax for creating an object in JavaScript that is composed of key-value pairs
+*/
+
+// Example of singleton
+const singletonObject = new Object();
+singletonObject.id ="1";
+singletonObject.name = "Nikhil"
+singletonObject.age="22"
+
+console.log(singletonObject);
+
+// Example of literals
+const literalsObject = {
+    "id":1,
+    "Name" : "nikhil"
+}
+console.log(literalsObject);
+
+/* Access object in two types */
+// First way
+console.log(literalsObject.Name);
+
+// Second way
+console.log(literalsObject["id"]);
+
+/* We can nested the objects into objects*/
+// For example 
+const nestedObjectExample = {
+  id:1,
+  name:"Nikhil",
+  Skills:{
+    languages: ["C", "c++"],
+    Database : "mongoDB"
+  }
+}
+console.log(nestedObjectExample);
+console.log(nestedObjectExample.Skills.languages);
+
+// Combine two object in one like merge of an array
+/* Syntax -> Object.assign(Target, source)*/
+const resultOfCombineObj = Object.assign({},nestedObjectExample, singletonObject)
+console.log(resultOfCombineObj);
+
+/* Some another properties of object that is we can freeze the object
+Now you make an object and you want the value doesn't change of that object then
+we have to freeze the object with current value
+*/
+// For example
+const freezeTheObject = {
+  id :5,
+  name: "nikhil"
+}
+
+// Freeze this object 
+Object.freeze(freezeTheObject);
+console.log(freezeTheObject); // { id: 5, name: 'nikhil' }
+
+// Try to manipulate the object
+freezeTheObject.id=6;
+console.log(freezeTheObject.id); // Still print the id= 5;
+
+// Return true becaue object is froozen 
+console.log(Object.isFrozen());
+
+// Use of symbol key in objects
+
+/* Symbol is use to make unique variables data 
+ if two variable contains same value but variables declared with symbol then they will be uniquely
+*/
+const symbolFirst = Symbol("Nikhil");
+const symbolSecond = Symbol("Nikhil");
+
+const result = symbolFirst === symbolSecond;
+console.log(result); // Is give false 
+
+// Creating symbols
+const mySymbol = Symbol('description');
+
+// Using symbols as object keys
+const objectForSymbol = {
+  [mySymbol]: 'Unique'
+};
+
+console.log(objectForSymbol[mySymbol]);
